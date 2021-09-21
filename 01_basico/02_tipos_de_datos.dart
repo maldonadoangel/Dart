e890f86418c41ddf7ee9ba6272cddacba8f5fbfa
Angel Hernan Morales Maldonado <maldonadoangel7069@gmail.com>
2021-09-21 02:28:23 -0600

    Tipos String

--- 01_basico/02_tipos_de_datos.dart
+++ 01_basico/02_tipos_de_datos.dart
@@ -1,13 +1,25 @@
 main(){
   // ===== Numeros
-  int a = 10;
-  double b = 3.2;
-  int numero1 = 30;
-  double numero2 = 3.2;
+int a = 10;
+double b = 3.2;
+int numero1 = 30;
+double numero2 = 3.2;
   //Null saving
-  int? c = null;
-  //No podemos declarar un valor entero como nulo sin el signo de interrogacion
+int? c = null;
+//No podemos declarar un valor entero como nulo sin el signo de interrogacion
  // int c = null;
-  print(c);
-  print(numero1 + numero2);
-}
+/* print(c); */
+/* print(numero1 + numero2);*/
+
+  /* Tipos Strings */
+String nombre = 'Angel Hernan';
+String? apellido = null;
+String nombre2 = "Angel Hernan";
+String nombreEspecial = "O\'Connor";
+
+print(nombre);
+print(apellido);
+print(nombre == nombre2);
+print(nombreEspecial);
+print('Hola, que tal estas? $nombre');
+} 
