a488c6fb314ead4904e40b874bac76d82deb265d
Angel Hernan Morales Maldonado <maldonadoangel7069@gmail.com>
2021-09-21 02:02:03 -0600

    Tipos de variables numeros

---
+++ 01_basico/02_tipos_de_datos.dart
@@ -0,0 +1,13 @@
+main(){
+  // ===== Numeros
+  int a = 10;
+  double b = 3.2;
+  int numero1 = 30;
+  double numero2 = 3.2;
+  //Null saving
+  int? c = null;
+  //No podemos declarar un valor entero como nulo sin el signo de interrogacion
+ // int c = null;
+  print(c);
+  print(numero1 + numero2);
+}
