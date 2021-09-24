c5f37ed77d017f00741b5ee3e79c1a7cb6c577e0
Angel Morales <maldonadoangel7069@gmail.com>
2021-09-24 17:39:40 -0500

    Tipos de dato: Arreglo

---
+++ 01_basico/03_tipos_de_datos_listas.dart
@@ -0,0 +1,11 @@
+
+
+main(){
+  //Listas, un arreglo es un contenedor que nos permite guardar muchos elementos relacionados entre si
+  List<String> villanos = ['Lex lutor', 'DOOM', 'Capitan America']; // los arreglos comienzan en el index 0
+  //List<String> villanos2 = new List();
+
+  villanos[0] = 'SuperMan';
+  villanos[2] = 'Craneo Rojo';
+  print(villanos);
+}
