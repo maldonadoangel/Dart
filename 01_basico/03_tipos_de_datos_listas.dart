f7e2d1b7ef3b97af2fc58a73f415752013a014ad
Angel Morales <maldonadoangel7069@gmail.com>
2021-09-24 17:56:02 -0500

    Tipo de datos Set

--- 01_basico/03_tipos_de_datos_listas.dart
+++ 01_basico/03_tipos_de_datos_listas.dart
@@ -7,5 +7,26 @@ main(){
 
   villanos[0] = 'SuperMan';
   villanos[2] = 'Craneo Rojo';
+  villanos.add('Duende verde');
+  villanos.add('Duende verde');
+  villanos.add('Duende verde');
+  villanos.add('Duende verde');
+  villanos.add('Duende verde');
   print(villanos);
+  //Convertimos una lista en un set y luego ese set de nuevo a una lista, esto para eliminar los valores duplicados
+  var villanosSet = villanos.toSet();
+  print(villanosSet);
+  var villanosLista = villanosSet.toList();
+  print(villanosLista); 
+
+
+  // ================ SETS, puede ser iterable igual que las listas, pero los sets no permite tener valores duplicados
+
+  Set<String> villanos3 = {'Lex lutor', 'DOOM', 'Capitan America'};
+  villanos3.add('Duende verde');
+  villanos3.add('Duende verde');
+  villanos3.add('Duende verde');
+  villanos3.add('Duende verde');
+  villanos3.add('Duende verde');
+  print(villanos3);
 }
