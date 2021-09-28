73af23ea4506b1c90e85589f7ff9d77dfaa8023f
Angel Morales <maldonadoangel7069@gmail.com>
2021-09-27 22:19:04 -0500

    Operadores condicionales

---
+++ 01_basico/05_operadores_matematicos.dart
@@ -0,0 +1,16 @@
+
+
+main(){
+  /*Un operador Matematico es aquel que indica a la computadora que debe realizar una tarea matematica */
+
+  int suma = 10 +13;
+  int resta = 30 - 10;
+  int multiplicacion = 10 * 10; 
+  int? numero1;
+
+  print(suma);
+  print(resta);
+  print(multiplicacion);
+  print(numero1);
+
+}
