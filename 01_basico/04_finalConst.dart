5bf8f0058d6f63d66f92c8466fb958bf2033872b
Angel Morales <maldonadoangel7069@gmail.com>
2021-09-27 21:34:36 -0500

    Final vs Const

---
+++ 01_basico/04_finalConst.dart
@@ -0,0 +1,22 @@
+main(){
+  var a = 10;
+  final double b = 10;
+  const double c = 10;
+
+  //Los tipos de variables final, const, sirven para definir variables que no van a cambiar de valor una vez se inicien
+  // a = 30;
+  // b = 20;
+  // c = 15;
+
+  final List<String> personasFinal = ["Juan", "Pedro", "Maria"];
+  const List<String> personasConst = ["Juancho", "peter", "Mary Jane"];
+
+  personasFinal.add('Mariano');
+
+  // personasConst.add('Mariano');
+
+
+  //Const es más estricta a la hora de modificar, ya que no permite de ninguna manera su modificacion, con final nos permite agregar datos a nuestro lista
+  print(personasFinal);
+  // print(personasConst); 
+}
