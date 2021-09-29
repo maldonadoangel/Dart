7d1a38a532e18666270169d36f498b0876ad28de
Angel Morales <maldonadoangel7069@gmail.com>
2021-09-29 17:12:03 -0500

    Funcion basica

---
+++ 03_funciones_basicas.dart/funcion_basica.dart
@@ -0,0 +1,16 @@
+
+main(){
+  
+  var mensaje = saludar();
+
+  print(mensaje);
+  
+  
+}
+
+saludar(){
+  print("Hola usuario");
+  print("Hola usuario");
+  print("Hola usuario");
+  return 'Fin de la funcion';
+}
