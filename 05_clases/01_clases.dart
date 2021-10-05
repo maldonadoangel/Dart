cab106abf53795d2f3da2c66e57a27131ef96c95
Angel Morales <maldonadoangel7069@gmail.com>
2021-10-05 18:25:26 -0500

    Propiedades privadas

--- 05_clases/01_clases.dart
+++ 05_clases/01_clases.dart
@@ -1,33 +1,11 @@
+import 'clases/persona.dart';
+
 main() {
-  // final persona = {
-  // 'nombre': 'Angel Hernan',
-  // 'apellido': 'Morales Maldonado',
-  // 'edad': 23,
-  // };
-
-  // print(persona['edad']);
-
   //Instancia de la clase
   final Persona persona = new Persona();
   persona.nombre = 'Angel Hernan';
   persona.edad = 23;
-  persona.bio = 'Guatemala';
+  // persona.bio = 'Guatemala'; se volvio privado no lo podemos trabajar por ahora
 
   print(persona);
-}
-
-//Clases
-class Persona {
-  //campos, propiedades o atributos
-  String? nombre;
-  int? edad;
-  String? bio;
-
-  //Constructores
-
-  //Gets y Sets
-
-  //Metodos
-  @override
-  String toString() => '$nombre $edad años, nacio en $bio';
 }
---
+++ 05_clases/clases/persona.dart
@@ -0,0 +1,15 @@
+//Clases
+class Persona {
+  //campos, propiedades o atributos
+  String? nombre;
+  int? edad;
+  String? _bio;
+
+  //Constructores
+
+  //Gets y Sets
+
+  //Metodos
+  @override
+  String toString() => '$nombre $edad años, nacio en $_bio';
+}
