296bc70de0521fe4968b3132766e9c166197f4eb
Angel Morales <maldonadoangel7069@gmail.com>
2021-10-05 21:07:22 -0500

    Constructores basicos

--- 05_clases/01_clases.dart
+++ 05_clases/01_clases.dart
@@ -2,10 +2,9 @@ import 'clases/persona.dart';
 
 main() {
   //Instancia de la clase
-  final Persona persona = new Persona();
-  persona.nombre = 'Angel Hernan';
-  persona.edad = 23;
+  final Persona persona =
+      new Persona(edad: 23, nombre: 'Angel Hernan Morales Maldonado');
   // persona.bio = 'Guatemala'; se volvio privado no lo podemos trabajar por ahora
-
+  //persona.bio = 'Hola estoy siendo cambiado por un setter';
   print(persona);
 }
--- 05_clases/clases/persona.dart
+++ 05_clases/clases/persona.dart
@@ -3,11 +3,28 @@ class Persona {
   //campos, propiedades o atributos
   String? nombre;
   int? edad;
-  String? _bio;
+  String _bio = 'Hola soy una propiedad privada';
 
   //Constructores
+  // Persona(int edad, String nombre) {
+  // print('Constructor');
+  // _bio = 'Hola soy un constructor desde la clase Persona';
+  // this.edad = edad;
+  // this.nombre = nombre;
+  // }
 
-  //Gets y Sets
+  Persona({this.edad = 0, this.nombre = 'Sin nombre'});
+
+  //Gets y Sets, podemos aplicarle metodos a nuestro getter
+  // String? get bio {
+  // return _bio.toUpperCase();
+  // }
+  //tambien se puede escribir como funcion de flechan
+  String get bio => _bio.toUpperCase();
+
+  set bio(String texto) {
+    _bio = texto;
+  }
 
   //Metodos
   @override
