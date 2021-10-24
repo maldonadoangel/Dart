d0be9c5e2c4c4bda5dc89a2035c55107b0e2621f
Angel Morales <maldonadoangel7069@gmail.com>
2021-10-24 02:20:45 -0500

    Extraer y usar el body de la peticion http

--- 07_paquetes/paquetes/bin/main.dart
+++ 07_paquetes/paquetes/bin/main.dart
@@ -5,6 +5,14 @@ import 'package:paquetes/paquetes.dart' as paquetes;
 void main(List<String> arguments) {
   final url = Uri.parse('https://reqres.in/api/users?page=2');
   http.get(url).then((res) {
-    print(res);
+    // print(res);
+    //Extraemos la informacion de el body de
+    //la peticion http
+    final body = json.decode(res.body);
+    print(body);
+    //Como obtener la informacion de nuestro httpp, establecemos que queremos ver, de manera manual
+    print('page: ${body['page']}');
+    print('per_page: ${body['per_page']}');
+    print('id del tecer elemento: ${body['data'][2]} ');
   });
 }
