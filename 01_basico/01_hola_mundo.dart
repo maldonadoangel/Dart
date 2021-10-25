c36ccb6945cc324e6411fe0b1c6d7923a18b5040
Angel Morales <maldonadoangel7069@gmail.com>
2021-10-24 23:28:51 -0500

    Constructores con nombres y mapas como inicializacion de los constructores

--- 01_basico/01_hola_mundo.dart
+++ 01_basico/01_hola_mundo.dart
@@ -1,3 +1,11 @@
-main(){
-  print('Hola Mundo');
+import 'dart:io';
+
+main() {
+  stdout.write('Ingresa tu nombre');
+  String? nombre = stdin.readLineSync();
+  Saludar(nombre!);
+}
+
+void Saludar(String nombre) {
+  print('Hola, $nombre');
 }
---
+++ 07_paquetes/paquetes/lib/classes/reqrest_respuesta.dart
@@ -0,0 +1,32 @@
+// To parse this JSON data, do
+//
+//     final reqResRespuesta = reqResRespuestaFromJson(jsonString);
+
+import 'dart:convert';
+
+ReqResRespuesta reqResRespuestaFromJson(String str) =>
+    ReqResRespuesta.fromJson(json.decode(str));
+
+String reqResRespuestaToJson(ReqResRespuesta data) =>
+    json.encode(data.toJson());
+
+class ReqResRespuesta {
+  String? greeting;
+  List<String>? instructions;
+
+  ReqResRespuesta({
+    this.greeting,
+    this.instructions,
+  });
+
+  factory ReqResRespuesta.fromJson(Map<String, dynamic> json) =>
+      ReqResRespuesta(
+        greeting: json["greeting"],
+        instructions: List<String>.from(json["instructions"].map((x) => x)),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "greeting": greeting,
+        "instructions": List<dynamic>.from(instructions!.map((x) => x)),
+      };
+}
